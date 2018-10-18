4d83985ca99ecf9fb5e17e8cea1580ebeab56b30
dbarriof <dbarriof@dbarriof-PC>
2018-10-18 22:08:09 +0200

    Inicio ejercicio 3

---
+++ Ud2/PSP02Ejer3_1Join/src/NewHiloMain.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dbarriof
+ */
+public class NewHiloMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //NewHilo h1 = new NewHilo("hilo1");
+        //NewHilo h2 = new NewHilo("hilo2");
+        //NewHilo h3 = new NewHilo("hilo3");
+        
+        System.out.println(Thread.currentThread().getName());
+        /*System.out.println(h1.isAlive());
+        System.out.println(h2.isAlive());
+        System.out.println(h3.isAlive());
+        
+        h1.join();
+        h2.join();
+        h3.join();
+        
+        */System.out.println(Thread.currentThread().getName());/*
+        System.out.println(h1.isAlive());
+        System.out.println(h2.isAlive());
+        System.out.println(h3.isAlive());*/
+        
+    }
+    
+}
