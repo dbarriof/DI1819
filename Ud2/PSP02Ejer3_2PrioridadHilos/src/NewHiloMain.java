b38dd9e79d1695fd005fe4764cb043f29eaa17bd
dbarriof <dbarriof@dbarriof-PC>
2018-10-20 18:54:24 +0200

    Retocado ejercicio 3_2

--- Ud2/PSP02Ejer3_2PrioridadHilos/src/NewHiloMain.java
+++ Ud2/PSP02Ejer3_2PrioridadHilos/src/NewHiloMain.java
@@ -18,49 +18,45 @@ public class NewHiloMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ASIGNA PRIORIDAD MAXIMA A THREAD ACTUAL");
+        //System.out.println("ASIGNA PRIORIDAD MAXIMA A THREAD ACTUAL");
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         
-        System.out.println("CREO HILOS");
+        //System.out.println("CREO HILOS");
         MiHilo hiloLOW = new MiHilo(Thread.NORM_PRIORITY +2, "hiloLOW");
         MiHilo hiloHIGH = new MiHilo(Thread.NORM_PRIORITY -2, "hiloHIGH");
     
-        System.out.println("INICIO HILOS SECUNDARIOS");
+        //System.out.println("INICIO HILOS SECUNDARIOS");
         hiloLOW.start();
         hiloHIGH.start();
         
-        System.out.println("DUERMO HILO MAIN");
+        //System.out.println("DUERMO HILO MAIN");
         try {
-            Thread.sleep(10);
+            Thread.sleep(1000);
         } catch (InterruptedException ex) {}
         
-        System.out.println("PARO HILOS SECUNDARIOS");
+        //System.out.println("PARO HILOS SECUNDARIOS");
         hiloLOW.stop();
         hiloHIGH.stop();
         
-        System.err.println("ESTADO HILOS SECUNDARIOS");
-        System.out.println(hiloLOW.t.isAlive());
-        System.out.println(hiloHIGH.t.isAlive());
+        //System.err.println("ESTADO HILOS SECUNDARIOS");
+        System.out.println(hiloLOW.t.isAlive() +" "+ hiloLOW.t.getState());
+        System.out.println(hiloHIGH.t.isAlive()+" "+  hiloHIGH.t.getState());
         
-        System.out.println("ASIGNO CPU A HILO LOW");
+        //System.out.println("ASIGNO CPU A HILO LOW");
         try {
             hiloLOW.t.join();
         } catch (InterruptedException ex) {}
         
-        System.out.println("ASIGNO CPU A HILO HIGH");
+        //System.out.println("ASIGNO CPU A HILO HIGH");
         try {
             hiloHIGH.t.join();
         } catch (InterruptedException ex) {}
              
-        System.err.println("ESTADO HILOS SECUNDARIOS");
-        System.out.println(hiloLOW.t.isAlive());
-        System.out.println(hiloHIGH.t.isAlive());
+        //System.err.println("ESTADO HILOS SECUNDARIOS");
+        System.out.println(hiloLOW.t.isAlive()+" "+ hiloLOW.t.getState());
+        System.out.println(hiloHIGH.t.isAlive()+" "+  hiloHIGH.t.getState());
         
-        System.err.println("ESTADO HILOS SECUNDARIOS");
-        System.out.println(hiloLOW.t.getState());
-        System.out.println(hiloHIGH.t.getState());
-        
-        System.err.println("CONTADOR DE EJECUCIONES");
+        //System.err.println("CONTADOR DE EJECUCIONES");
         System.out.println( hiloLOW.contador );
         System.out.println( hiloHIGH.contador );
         
