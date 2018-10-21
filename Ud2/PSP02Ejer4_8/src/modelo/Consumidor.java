1a98a4c133acaf9c588d82e6d91339fcd1b25d57
dbarriof <dbarriof@dbarriof-PC>
2018-10-21 23:12:21 +0200

    Finalizado ejercicio

---
+++ Ud2/PSP02Ejer4_8/src/modelo/Consumidor.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dbarriof
+ */
+public class Consumidor extends Thread{
+    DatosCompartidos compartido;
+    String nombre;
+
+    public Consumidor(DatosCompartidos compartido, String nombre) {
+        this.compartido = compartido;
+        this.nombre = nombre;
+        this.setName(nombre);
+    }
+    
+    @Override
+    public void run(){
+        int i;
+        for (i = 1; i <= 5; i++) {
+            //while (compartido.getCadena() != null) {           
+                compartido.setCadena(nombre + String.valueOf(i));
+                System.out.println("Consumidor: " + nombre + " Iteración: " + i);
+                /*notify();
+            } 
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+            }*/
+        }
+    }
+}
+
